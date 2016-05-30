6a17da9db80ed5bc6b9323f466873434b1e80038
wstewart <william.stewart@furman.edu>
2016-05-30 17:11:57 -0400

    Attempting two node workers

--- NodeService/src/UDPServer.java
+++ NodeService/src/UDPServer.java
@@ -2,25 +2,31 @@ import java.io.*;
 import java.net.*;
 
 public class UDPServer {
+	static String nodeName = "Unkown node";
 	
 	public static void createServer() throws Exception{
 		try{
 		DatagramSocket serverSocket = new DatagramSocket(9875);
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
+		
 		while(true){
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
 			
 			DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
 			serverSocket.receive(receivePacket);
 			String sentence = new String(receivePacket.getData());
 			InetAddress IPAddress = receivePacket.getAddress();
+			InetAddress localAddr;
+			localAddr = InetAddress.getLocalHost();
+			nodeName = localAddr.getHostName();
 			int port = receivePacket.getPort();
-			String modifiedSentence = "Made it to worker: " + sentence ;
+			String modifiedSentence = "Made it to worker(" + nodeName + "): " + sentence ;
 			System.out.println(modifiedSentence);
 			sendData = modifiedSentence.getBytes();
 			DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,IPAddress,port);
 			serverSocket.send(sendPacket);
 			modifiedSentence = "";
+			sentence = "";
 		}
 		}
 		catch(SocketException ex){
